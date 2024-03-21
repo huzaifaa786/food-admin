22df6b5ad33ae54f9f64d2d1914fa689bf3bea06
Huzaifa <tritechuzaifa@gmail.com>
2024-03-21 10:45:13 +0500

    phone-validation

--- app/Http/Requests/UserCreateRequest.php
+++ app/Http/Requests/UserCreateRequest.php
@@ -25,7 +25,7 @@ class UserCreateRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'phone' => 'required|phone|unique:users,phone',
+            'phone' =>  'required|unique:users',
             'password' => 'required'
         ];
     }
