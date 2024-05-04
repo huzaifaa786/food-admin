98cc4a1a9845ee0f5284e173ea898c02848fea1c
Huzaifa <tritechuzaifa@gmail.com>
2024-05-04 11:50:30 +0500

    changes

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -173,7 +173,7 @@ class AuthController extends Controller
     {
         $existingEmail = User::where('email', $request->email)->first();
         if ($existingEmail) {
-            return Api::setResponse('Existing User', $existingEmail->withToken());
+            return Api::setResponse('Existing User', $existingEmail);
         } else {
             return Api::setError('Email is not exist');
         }
