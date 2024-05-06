ecc7eaa772eed456b233988c65726c27398affd3
Huzaifa <tritechuzaifa@gmail.com>
2024-05-06 12:03:13 +0500

    chagnge

--- app/Http/Controllers/Api/User/HomeController.php
+++ app/Http/Controllers/Api/User/HomeController.php
@@ -13,23 +13,17 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $categories = Category::with(['restaurants' => function ($query) {
-            $query->has('menu_categories')->withAvg('ratings', 'rating');
+        $categories = Category::all();
+        $restaurants = Category::has('restaurants.menu_categories')->with(['restaurants' => function ($query) {
+            $query->withAvg('ratings as rating', 'rating');
         }])->get();
 
-        $categoriesWithRestaurants = $categories->map(function ($category) {
-            $category->restaurants->transform(function ($restaurant) {
-                $restaurant->rating = $restaurant->ratings_avg_rating;
-                unset($restaurant->ratings_avg_rating);
-                return $restaurant;
-            });
-            return $category;
-        });
 
         $response = new stdClass();
         $response->categories = $categories;
-        $response->restaurants = $categoriesWithRestaurants;
+        $response->restaurants = $restaurants;
 
         return Api::setResponse('response', $response);
+
     }
 }
