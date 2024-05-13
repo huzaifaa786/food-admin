4d1fcd1e700cfbf590858c2b5a1ed01c37641d1d
Huzaifa <tritechuzaifa@gmail.com>
2024-05-13 16:01:34 +0500

    ad

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -24,4 +25,9 @@ class Notification extends Model
     protected $casts = [
         'seen' => 'boolean'
     ];
+
+    public function getCreatedAt($value)
+    {
+        return Carbon::parse($value);
+    }
 }
