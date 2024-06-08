bcf9c2884ba4236ac5f59dd61cdd080df1df61f7
AbRehman <tritechuzaifa@gmail.com>
2024-06-08 12:35:32 +0500

    hello

--- app/Http/Controllers/Api/User/HomeController.php
+++ app/Http/Controllers/Api/User/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Helpers\Api;
+use App\Helpers\LocationHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Poster;
 use App\Models\Restraunt;
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
 use stdClass;
 
@@ -14,19 +16,30 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
-        $restaurants = Category::has('restaurants.menu_categories')->with(['restaurants' => function ($query) {
-            $query->withAvg('ratings as rating', 'rating');
-        }])->get();
+        $categories = Category::with([
+            'restaurants' => function ($query) {
+                $query->withAvg('ratings as rating', 'rating');
+            }
+        ])->has('restaurants.menu_categories')->get();
 
         $posters = Poster::all();
+
+        $address = UserAddress::where('user_id', auth()->user()->id)->first();
+
+        if ($address) {
+            foreach ($categories as $category) {
+                $category->restaurants = $category->restaurants->filter(function ($restaurant) use ($address) {
+                    $distance = LocationHelper::calculateDistance($address->lat, $address->lng, $restaurant->lat, $restaurant->lng);
+                    return $distance <= ($restaurant->radius * 1000);
+                })->values(); // Reset keys after filtering
+            }
+        }
 
         $response = new stdClass();
         $response->categories = $categories;
         $response->posters = $posters;
-        $response->restaurants = $restaurants;
 
         return Api::setResponse('response', $response);
-
     }
+
 }
