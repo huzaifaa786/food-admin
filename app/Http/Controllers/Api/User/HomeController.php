deae770d6fbce59e17a2de288c5c58f350ab47c9
huzaifa <ihuzaifaaslam@gmail.com>
2024-12-31 12:54:55 +0500

    adsfasdf

--- app/Http/Controllers/Api/User/HomeController.php
+++ app/Http/Controllers/Api/User/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
                     $query->withAvg('ratings as rating', 'rating');
                 }
             ])
-            ->toSql();
+            ->get();
 
             dd($restaurants);
         $posters = Poster::whereHas('restraunt', function ($query) use ($address) {
