8542c8ad26316309f3309da97f57f3b0623be555
Mubeen Malik <mubeenmalik2829@gmail.com>
2025-03-07 11:31:03 +0500

    dasd

--- app/Http/Controllers/Api/User/HomeController.php
+++ app/Http/Controllers/Api/User/HomeController.php
@@ -25,9 +25,7 @@ class HomeController extends Controller
                 $query->where('status', RestrauntStatus::OPENED->value)
                     ->whereHas('menu_categories')
                     ->withAvg('ratings as rating', 'rating')
-                    ->whereRaw("
-                    " . LocationHelper::calculateDistanceSql($address->lat, $address->lng, 'restraunts.lat', 'restraunts.lng') . " <= restraunts.radius * 1000
-                ");
+                ;
             }
         ])->get();
 
