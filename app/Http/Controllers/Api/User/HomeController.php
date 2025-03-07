b99628f77dd5acb51647da5cd6ba82228f506616
Mubeen Malik <mubeenmalik2829@gmail.com>
2025-03-07 11:32:31 +0500

    dasda

--- app/Http/Controllers/Api/User/HomeController.php
+++ app/Http/Controllers/Api/User/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
 
         if ($address) {
             $restaurants = Restraunt::whereRaw("
-                (" . LocationHelper::calculateDistanceSql($address->lat, $address->lng, 'restaurants.lat', 'restaurants.lng') . " <= restaurants.radius * 1000)
+                (" . LocationHelper::calculateDistanceSql($address->lat, $address->lng, 'restraunts.lat', 'restraunts.lng') . " <= restraunts.radius * 1000)
             ")
                 ->where('status', RestrauntStatus::OPENED->value)
                 ->with(['menu_categories', 'category']) // Load category for each restaurant
