abbc966b6c2cfeb3b30b8198246a997c6cba7bf5
Mubeen Malik <mubeenmalik2829@gmail.com>
2025-03-08 11:54:06 +0500

    dasd

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -14,8 +14,7 @@ class MenuItem extends Model
     use HasFactory;
 
     protected $appends = [
-        'original_price',
-        'final_price'
+        'original_price'
     ];
 
     protected $fillable = [
@@ -42,28 +41,40 @@ class MenuItem extends Model
     {
         $currentDate = now()->toDateString();
 
-        if ($this->discount && $this->discount_till_date && $currentDate <= $this->discount_till_date) {
-            return round($value - ($value * ($this->discount / 100)), 2);
+        if (
+            $this->discount &&
+            $this->discount_till_date &&
+            $currentDate <= $this->discount_till_date
+        ) {
+            return $value - ($value * ($this->discount / 100));
+        } else {
+            $this->original_price = null;
+            $this->discount = 0.0;
+            $this->discount_till_date = null;
+            $this->discount_days = '0';
         }
 
-        // If discount has expired, reset to original price
-        return round($this->attributes['price'], 2);
+        return $value;
     }
 
     public function getOriginalPriceAttribute()
     {
         $currentDate = now()->toDateString();
 
-        if ($this->discount && $this->discount_till_date && $currentDate <= $this->discount_till_date) {
-            return round($this->attributes['price'], 2);
+        if (
+            $this->discount &&
+            $this->discount_till_date &&
+            $currentDate <= $this->discount_till_date
+        ) {
+            return $this->price / (1 - $this->discount / 100);
+        } else {
+            $this->original_price = null;
+            $this->discount = 0.0;
+            $this->discount_till_date = null;
+            $this->discount_days = '0';
         }
 
-        return null; // No discount applied
-    }
-
-    public function getFinalPriceAttribute()
-    {
-        return $this->price; // This will return the final price, which is either discounted or original
+        return null; // Return null if there's no discount
     }
 
     /**
